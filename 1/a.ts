1b6b366de6301c5cb6571fd4d8c7aa7dda115733
Spike <l-yanlei@hotmail.com>
2022-12-02 03:07:43 +0800

    feat: init

---
+++ 1/a.ts
@@ -0,0 +1,15 @@
+import { readLines } from "https://deno.land/std@0.167.0/io/buffer.ts";
+let fileReader = await Deno.open("./input-a.txt");
+let sum = 0
+let max = 0
+
+for await (let line of readLines(fileReader)) {
+  if (line !== "") {
+    sum += +line
+  } else {
+    max = Math.max(max, sum)
+    sum = 0
+  }
+}
+
+console.log({ max })
