a28d4aeca1e0f06de1b70f93e6d2e8c8de9b88c6
Spike <l-yanlei@hotmail.com>
2022-12-03 15:37:15 +0800

    feat: 2a

--- puzzle/day2/a.ts
+++ puzzle/day2/a.ts
@@ -1,9 +1,64 @@
 import { readLines } from "https://deno.land/std@0.167.0/io/buffer.ts";
 let fileReader = await Deno.open("./input.txt");
-let score = 0
+let totalScore = 0
 
-for await (let line of readLines(fileReader)) {
-
+const MATCH_SCORE = {
+  LOSE: 0,
+  DRAW: 3,
+  WIN: 6
+}
+const BASIC_SCORE = {
+  // Rock
+  X: 1,
+  // Paper
+  Y: 2,
+  // Scissors
+  Z: 3
+}
+const X_SCORE = {
+  A: MATCH_SCORE.DRAW,
+  B: MATCH_SCORE.LOSE,
+  C: MATCH_SCORE.WIN,
 }
 
-console.log(score)
+const Y_SCORE = {
+  A: MATCH_SCORE.WIN,
+  B: MATCH_SCORE.DRAW,
+  C: MATCH_SCORE.LOSE,
+}
+
+const Z_SCORE = {
+  A: MATCH_SCORE.LOSE,
+  B: MATCH_SCORE.WIN,
+  C: MATCH_SCORE.DRAW,
+}
+
+/**
+ * A: Rock B: Paper C: Scissors
+ * X: Rock Y: Paper Z: Scissors
+ */
+function calcScoreInOneRound(strategy: string) {
+  const [opponent, mine] = strategy.split(" ") as [keyof typeof X_SCORE, keyof typeof BASIC_SCORE]
+
+  let score = BASIC_SCORE[mine]
+
+  switch (mine) {
+    case 'X':
+      score += X_SCORE[opponent]
+      break
+    case 'Y':
+      score += Y_SCORE[opponent]
+      break
+    case 'Z':
+      score += Z_SCORE[opponent]
+      break
+  }
+
+  return score
+}
+
+for await (let line of readLines(fileReader)) {
+  totalScore += calcScoreInOneRound(line)
+}
+
+console.log(totalScore)
