73f15b55ac036427d133e95491b15cc42023dfd9
Spike <l-yanlei@hotmail.com>
2022-12-04 15:00:39 +0800

    feat: day4

---
+++ puzzle/day4/b.ts
@@ -0,0 +1,21 @@
+import { readLines } from "https://deno.land/std@0.167.0/io/buffer.ts";
+const fileReader = await Deno.open("./input.txt");
+
+let count = 0
+
+function isPairOverlap(pairs: string): boolean {
+  const [pair1, pair2] = pairs.split(",")
+  const [start1, end1] = pair1.split("-").map((s) => +s)
+  const [start2, end2] = pair2.split("-").map((s) => +s)
+
+  const pair1ContainsPair2 = start1 <= start2 && start2 <= end1
+  const pair2ContiansPair1 = start2 <= start1 && start1 <= end2
+
+  return pair1ContainsPair2 || pair2ContiansPair1
+}
+
+for await (let line of readLines(fileReader)) {
+  count += isPairOverlap(line) ? 1 : 0
+}
+
+console.log({ count })
