078ad8d2c2d8df1f1178ea4000a57d8cc8c239df
Spike <l-yanlei@hotmail.com>
2022-12-05 22:19:22 +0800

    refactor: support read different input file

--- puzzle/utils.ts
+++ puzzle/utils.ts
@@ -1,12 +1,12 @@
 import { readLines } from "https://deno.land/std@0.167.0/io/buffer.ts";
 import { fromFileUrl, dirname, join } from "https://deno.land/std@0.167.0/path/posix.ts";
 
-export async function getInputInterator(meta: ImportMeta) {
-  const path = getInputPath(meta)
+export async function getInputInterator(meta: ImportMeta, inputFile = "input.txt") {
+  const path = getInputPath(meta, inputFile)
   const fileReader = await Deno.open(path);
   return readLines(fileReader)
 }
 
-function getInputPath(meta: ImportMeta) {
-  return fromFileUrl(join(dirname(meta.url), 'input.txt'))
+function getInputPath(meta: ImportMeta, inputFile: string) {
+  return fromFileUrl(join(dirname(meta.url), inputFile))
 }
