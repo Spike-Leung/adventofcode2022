b37731d68c6673c3dce1721962446a4951034508
Spike <l-yanlei@hotmail.com>
2022-12-08 00:31:36 +0800

    feat: day7 part2

--- puzzle/day7/a.ts
+++ puzzle/day7/a.ts
@@ -1,82 +1,4 @@
-import { getInputInterator } from "@/utils.ts"
-
-type Dir = Record<string, number | object>
-
-const cwd: string[] = []
-const root: Dir = {}
-
-const getPath = (cmd: string) => {
-  const match = cmd.match(/\$ cd (.*)/)
-
-  return match ? match[1] : ""
-}
-
-const isList = (cmd: string) => {
-  return cmd === "$ ls"
-}
-
-const isCmd = (line: string) => {
-  return line.indexOf("$") === 0
-}
-
-const isDir = (line: string) => {
-  return line.indexOf("dir") === 0
-}
-
-const getDirPath = (line: string) => {
-  const match = line.match(/dir (.*)/)
-  return match ? match[1] : ""
-}
-
-const getKeyAndSize = (line: string) => {
-  const match = line.match(/(\d+) (.*)/)
-  const [_, size = 0, key = ""] = match ?? []
-
-  return { key, size: +size }
-}
-
-const getCwdDir = () => {
-  let obj: Dir = root
-
-  for (let k of cwd) {
-    obj = obj[k] as Dir
-  }
-
-  return obj
-}
-
-const appendToRoot = (key: string, size: number | object) => {
-  const dir = getCwdDir()
-  dir[key] = size
-}
-
-for await (let line of await getInputInterator(import.meta)) {
-  if (isCmd(line)) {
-    if (isList(line)) {
-      continue
-    } else {
-      const path = getPath(line)
-
-      switch (path) {
-        case "/":
-          break
-        case "..":
-          cwd.pop()
-          break
-        default:
-          cwd.push(path)
-          break
-      }
-    }
-  } else {
-    if (isDir(line)) {
-      appendToRoot(getDirPath(line), {})
-    } else {
-      const { key, size } = getKeyAndSize(line)
-      appendToRoot(key, size)
-    }
-  }
-}
+import rootDir from "./util.ts"
 
 function calcTotal() {
   let sum = 0
@@ -95,7 +17,7 @@ function calcTotal() {
     return dirTotal
   }
 
-  dfs(root)
+  dfs(rootDir)
 
   return sum
 }
--- puzzle/day7/b.ts
+++ puzzle/day7/b.ts
@@ -1,5 +1,39 @@
-import { getInputInterator } from "@/utils.ts"
+import rootDir from "./util.ts"
 
-for await (let line of await getInputInterator(import.meta)) {
+const TOTAL_SPACE = 70000000
+const NEED_UNUSED_SPACE = 30000000
 
+// how many space need to delete
+
+
+const flatDir: Record<string, number> = {}
+
+function buildFlatDir() {
+  const dfs = (key: string, data: number | object): number => {
+    if (typeof data === "number") {
+      return data
+    }
+
+    const dirTotal = Object.entries(data).reduce((acc, entry) => {
+      const [key, value] = entry
+      acc += dfs(key, value)
+      return acc
+    }, 0)
+
+    flatDir[key] = dirTotal
+
+    return dirTotal
+  }
+
+  dfs('root', rootDir)
 }
+
+buildFlatDir()
+
+const rootTotalSize = flatDir.root
+const currentUnusedSize = TOTAL_SPACE - rootTotalSize
+const leastSizeToRemove = NEED_UNUSED_SPACE - currentUnusedSize
+
+delete flatDir.root
+const smallestDirSizeToRemove = Object.values(flatDir).filter((v) => v >= leastSizeToRemove).sort((a, b) => a - b)[0]
+console.log(smallestDirSizeToRemove)
