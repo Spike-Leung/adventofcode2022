8042f390a47da5e86b5ba31de34abc818d52bd3c
Spike <l-yanlei@hotmail.com>
2022-12-10 19:18:58 +0800

    feat: day10 part2

--- puzzle/day10/a.ts
+++ puzzle/day10/a.ts
@@ -1,5 +1,35 @@
 import { getInputInterator } from "@/utils.ts"
-import { sum, processCommand } from "./util.ts"
+import { extractAddValue } from "./util.ts"
+let register = 1
+let sum = 0
+const LOG_CYCLE = [20, 60, 100, 140, 180, 220]
+const target = {
+  val: 1
+}
+const proxyCycle = new Proxy(target, {
+  set(target, prop, value) {
+    target[prop] += 1
+    if (LOG_CYCLE.includes(target[prop])) {
+      sum += calcSignalLength(target[prop], register)
+    }
+    return true
+  }
+})
+
+const calcSignalLength = (cycle: number, register: number): number => {
+  return cycle * register
+}
+
+const processCommand = (line: string) => {
+  if (line === "noop") {
+    proxyCycle.val++
+  } else {
+    proxyCycle.val++
+    register += extractAddValue(line)
+    proxyCycle.val++
+  }
+}
+
 
 for await (let line of await getInputInterator(import.meta)) {
   processCommand(line)
--- puzzle/day10/util.ts
+++ puzzle/day10/util.ts
@@ -1,38 +1,5 @@
-let register = 1
-export let sum = 0
-const LOG_CYCLE = [20, 60, 100, 140, 180, 220]
-
-const target = {
-  val: 1
-}
-const proxyCycle = new Proxy(target, {
-  set(target, prop, value) {
-    target[prop] += 1
-    if (LOG_CYCLE.includes(target[prop])) {
-      console.log({ cycle: target[prop], sum: calcSignalLength(target[prop], register), register })
-      sum += calcSignalLength(target[prop], register)
-    }
-    return true
-  }
-})
-
-export const calcSignalLength = (cycle: number, register: number): number => {
-  return cycle * register
-}
-
 export const extractAddValue = (line: string): number => {
   const [mark, val] = line.split(" ")
 
   return +val
-}
-
-export const processCommand = (line: string) => {
-  if (line === "noop") {
-    proxyCycle.val++
-  } else {
-    proxyCycle.val++
-    console.log({ register, val: extractAddValue(line) })
-    register += extractAddValue(line)
-    proxyCycle.val++
-  }
 }
