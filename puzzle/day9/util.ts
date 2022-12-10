02c25c3a84c411cb56b2c7fe8d61dc0d94e13aa7
Spike <l-yanlei@hotmail.com>
2022-12-10 14:18:43 +0800

    feat: day9 part2

--- puzzle/day9/util.ts
+++ puzzle/day9/util.ts
@@ -37,9 +37,20 @@ const inSameRowOrCol = (head: Head, tail: Head) => {
   return dx === 0 || dy === 0
 }
 
+const getDirInSameRowOrCol = (head, tail) => {
+  const dx = head[0] - tail[0]
+  const dy = head[1] - tail[1]
+
+  if (dx === 0) {
+    return dy > 0 ? "D" : "U"
+  }
+
+  return dx > 0 ? "R" : "L"
+}
+
 export const moveTail = (dir: Dir, head: Head, tail: Head) => {
   if (inSameRowOrCol(head, tail)) {
-    move(dir, tail)
+    move(getDirInSameRowOrCol(head, tail), tail)
   } else {
     const rowDir = head[0] - tail[0] > 0 ? 'R' : 'L'
     const colDir = head[1] - tail[1] > 0 ? 'D' : 'U'
