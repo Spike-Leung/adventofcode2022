0e0deddd19cf2d55aab2f193e03b4e411cf02c67
Spike <l-yanlei@hotmail.com>
2022-12-14 02:20:58 +0800

    feat: day12 part2

--- puzzle/day12/a.ts
+++ puzzle/day12/a.ts
@@ -1,10 +1,8 @@
 import matrix from "./utils.ts"
+
 let start = [0, 0]
-let fewestSteps = 0
-let step = 1
 const rows = matrix.length
 const cols = matrix[0].length
-
 for (let r = 0; r < rows; r++) {
   for (let c = 0; c < cols; c++) {
     if (matrix[r][c] === "S") {
@@ -18,43 +16,51 @@ for (let r = 0; r < rows; r++) {
   }
 }
 
-const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-let currPositions = [start]
-let visited = new Set()
+export function findFewestSteps(start = [0, 0], matrix: string[][]) {
+  let fewestSteps = 0
+  let step = 1
+  const rows = matrix.length
+  const cols = matrix[0].length
+  const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  let currPositions = [start]
+  let visited = new Set()
 
-visited.add(start.join(','))
+  visited.add(start.join(','))
 
-while (currPositions.length && fewestSteps === 0) {
-  step++
-  let nextPositions = []
+  while (currPositions.length && fewestSteps === 0) {
+    step++
+    let nextPositions = []
 
-  for (let position of currPositions) {
-    const [x, y] = position
-    const currElevation = matrix[x][y]
+    for (let position of currPositions) {
+      const [x, y] = position
+      const currElevation = matrix[x][y]
 
-    for (const dir of dirs) {
-      const nx = x + dir[0]
-      const ny = y + dir[1]
-      const key = `${nx},${ny}`
+      for (const dir of dirs) {
+        const nx = x + dir[0]
+        const ny = y + dir[1]
+        const key = `${nx},${ny}`
 
-      if (nx < 0 || ny < 0 || nx >= rows || ny >= cols || visited.has(key)) {
-        continue
-      } else {
-        const nextElevation = matrix[nx][ny]
-        const canMove = nextElevation.charCodeAt(0) - currElevation.charCodeAt(0) <= 1
+        if (nx < 0 || ny < 0 || nx >= rows || ny >= cols || visited.has(key)) {
+          continue
+        } else {
+          const nextElevation = matrix[nx][ny]
+          const canMove = nextElevation.charCodeAt(0) - currElevation.charCodeAt(0) <= 1
 
-        if (canMove) {
-          visited.add(key)
-          nextPositions.push([nx, ny])
+          if (canMove) {
+            visited.add(key)
+            nextPositions.push([nx, ny])
 
-          if (nextElevation === "z") {
-            fewestSteps = step
+            if (nextElevation === "z") {
+              fewestSteps = step
+            }
           }
         }
       }
     }
+    currPositions = nextPositions
   }
-  currPositions = nextPositions
+
+  return fewestSteps
 }
 
-console.log(fewestSteps)
+console.log('part 1:', findFewestSteps(start, matrix))
--- puzzle/day12/b.ts
+++ puzzle/day12/b.ts
@@ -1,5 +1,30 @@
-import { getInputInterator } from "@/utils.ts"
+import matrix from "./utils.ts"
+import { findFewestSteps } from "./a.ts"
 
-for await (let line of await getInputInterator(import.meta)) {
+let startList = []
+let mostFewestSteps = Number.MAX_SAFE_INTEGER
+const rows = matrix.length
+const cols = matrix[0].length
+const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
+for (let r = 0; r < rows; r++) {
+  for (let c = 0; c < cols; c++) {
+    if (['a', 'S'].includes(matrix[r][c])) {
+      startList.push([r, c])
+      matrix[r][c] = 'a'
+    }
+
+    if (matrix[r][c] === "E") {
+      matrix[r][c] = 'z'
+    }
+  }
 }
+
+for (const start of startList) {
+  const fewestSteps = findFewestSteps(start, matrix)
+  if (fewestSteps > 0) {
+    mostFewestSteps = Math.min(mostFewestSteps, fewestSteps)
+  }
+}
+
+console.log('part2:', mostFewestSteps)
