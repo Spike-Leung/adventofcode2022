116241d27bba7c9b1f5794df51c184972f2dc4d6
Spike <l-yanlei@hotmail.com>
2022-12-14 02:03:07 +0800

    feat: day12 part1

--- puzzle/day12/a.ts
+++ puzzle/day12/a.ts
@@ -1,52 +1,60 @@
 import matrix from "./utils.ts"
-
 let start = [0, 0]
 let fewestSteps = 0
-let step = 0
+let step = 1
 const rows = matrix.length
 const cols = matrix[0].length
 
+for (let r = 0; r < rows; r++) {
+  for (let c = 0; c < cols; c++) {
+    if (matrix[r][c] === "S") {
+      start = [r, c]
+      matrix[r][c] = 'a'
+    }
+
+    if (matrix[r][c] === "E") {
+      matrix[r][c] = 'z'
+    }
+  }
+}
+
 const dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-let currElevation = [start]
+let currPositions = [start]
 let visited = new Set()
 
-visited.add('0,0')
+visited.add(start.join(','))
 
-while (currElevation.length) {
+while (currPositions.length && fewestSteps === 0) {
   step++
-  let nextElevation = []
+  let nextPositions = []
 
-  for (let elevation of currElevation) {
-    const [x, y] = elevation
-    const char = matrix[x][y]
-    const nextChar = String.fromCharCode(char.charCodeAt(0) + 1)
+  for (let position of currPositions) {
+    const [x, y] = position
+    const currElevation = matrix[x][y]
 
     for (const dir of dirs) {
       const nx = x + dir[0]
       const ny = y + dir[1]
       const key = `${nx},${ny}`
 
-      if (nx < 0 || ny < 0 || nx >= rows || ny >= cols) {
-        continue
-      }
-
-      if (visited.has(key)) {
+      if (nx < 0 || ny < 0 || nx >= rows || ny >= cols || visited.has(key)) {
         continue
       } else {
-        const currChar = matrix[nx][ny]
+        const nextElevation = matrix[nx][ny]
+        const canMove = nextElevation.charCodeAt(0) - currElevation.charCodeAt(0) <= 1
 
-        if (char === 'z' && currChar === "E") {
-          fewestSteps = step
-        }
-
-        if (currChar === char || currChar === nextChar) {
+        if (canMove) {
           visited.add(key)
-          nextElevation.push([nx, ny])
+          nextPositions.push([nx, ny])
+
+          if (nextElevation === "z") {
+            fewestSteps = step
+          }
         }
       }
     }
   }
-  currElevation = nextElevation
+  currPositions = nextPositions
 }
 
 console.log(fewestSteps)
