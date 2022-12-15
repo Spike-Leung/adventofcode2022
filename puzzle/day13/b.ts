63d095e41f9b96895a56034ea46deb1845d45a28
Spike <l-yanlei@hotmail.com>
2022-12-16 01:12:19 +0800

    feat: day13 part2

--- puzzle/day13/b.ts
+++ puzzle/day13/b.ts
@@ -1,5 +1,12 @@
-import { getInputInterator } from "@/utils.ts"
+import { pairs, compare } from "./util.ts"
 
-for await (let line of await getInputInterator(import.meta)) {
+const mark1 = [[2]]
+const mark2 = [[6]]
+pairs.push(mark1, mark2)
+pairs.sort((a, b) => compare(a, b) ? -1 : 1)
 
-}
+const index1 = pairs.findIndex((p) => p.toString() === mark1.toString())
+const index2 = pairs.findIndex((p) => p.toString() === mark2.toString())
+
+
+console.log({ index1, index2, res: (index1 + 1) * (index2 + 1) })
