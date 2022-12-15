0fa31f0bea96c09e5a3484084ac1d770d9f64aa1
Spike <l-yanlei@hotmail.com>
2022-12-16 01:03:27 +0800

    feat: day13 part1

--- puzzle/day13/a.ts
+++ puzzle/day13/a.ts
@@ -2,9 +2,15 @@ import { pairs, compare } from "./util.ts"
 
 let rightPairs = []
 for (let i = 0; i < pairs.length; i += 2) {
-  if (compare(pairs[i], pairs[i + 1])) {
-    rightPairs.push(i / 2 + 1)
-  }
+    const inorder = compare(pairs[i], pairs[i + 1])
+
+    console.log({ i: i / 2 + 1 , inorder })
+
+    if (compare(pairs[i], pairs[i + 1]) === true) {
+        rightPairs.push(i / 2 + 1)
+    }
 }
+
+console.log({ rightPairs })
 
 console.log(rightPairs.reduce((acc, cur) => acc += cur, 0))
--- puzzle/day13/util.ts
+++ puzzle/day13/util.ts
@@ -3,54 +3,68 @@ import { getInputInterator } from "@/utils.ts"
 export const pairs = []
 
 function parseLineToArray(line: string) {
-  const stack = []
+    const stack = []
 
-  for (let s of line) {
-    if (s === '[') {
-      stack.push([])
-    } else if (s === ',') {
-      continue
-    } else if (s === ']') {
-      if (stack.length > 1) {
-        const arr = stack.pop()
-        stack[stack.length - 1].push(arr)
-      }
-    } else {
-      stack[stack.length - 1].push(+s)
+    for (let i = 0; i < line.length; i++) {
+        const s = line[i]
+        if (s === '[') {
+            stack.push([])
+        } else if (s === ',') {
+            continue
+        } else if (s === ']') {
+            if (stack.length > 1) {
+                const arr = stack.pop()
+                stack[stack.length - 1].push(arr)
+            }
+        } else {
+            let num = ''
+            while (!Number.isNaN(+line[i])) {
+                num += line[i]
+                i++
+            }
+            stack[stack.length - 1].push(+num)
+            i--
+        }
     }
-  }
 
-  return stack.pop()
+
+    return stack.pop()
 }
 
-export function compare(a, b, oneTimeMatch = false) {
-  if (typeof a === "number" && typeof b === "number") {
-    return a <= b
-  } else if (typeof a === "number" && typeof b === "object") {
-    return compare([a], b, true)
-  } else if (typeof a === "object" && typeof b === "number") {
-    return compare(a, [b], true)
-  } else if (typeof a === "object" && typeof b === "object") {
-    if (oneTimeMatch) {
-      return compare(a[0], b[0])
-    }
-    if (b.length < a.length) {
-      return false
-    }
+export function compare(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+        if (a < b) {
+            return true
+        } else if (a === b) {
+            return 'continue'
+        } else {
+            return false
+        }
+    } else if (typeof a === "number" && typeof b === "object") {
+        return compare([a], b, true)
+    } else if (typeof a === "object" && typeof b === "number") {
+        return compare(a, [b], true)
+    } else if (typeof a === "object" && typeof b === "object") {
+        let res = 'continue'
 
-    let res = true
+        for (let i = 0; i < a.length && res === 'continue'; i++) {
+            if (b[i] === undefined) {
+                res = false
+                break;
+            }
+            res = compare(a[i], b[i])
+        }
 
-    for (let i = 0; i < a.length && res; i++) {
-      res = compare(a[i], b[i])
+        if (res === 'continue' && a.length < b.length) {
+            res = true
+        }
+
+        return res
     }
-
-    return res
-  }
 }
-
 
 for await (let line of await getInputInterator(import.meta)) {
-  if (line) {
-    pairs.push(parseLineToArray(line))
-  }
+    if (line) {
+        pairs.push(parseLineToArray(line))
+    }
 }
